440be10b348b250ef8106d30a8c92a99b45fa9ef
AyaSanae <39239316+AyaSanae@users.noreply.github.com>
2025-06-19 17:25:26 +0800

    fix: Server and client inventories out of sync after respawn_player (#914)

--- pumpkin/src/net/packet/play.rs
+++ pumpkin/src/net/packet/play.rs
@@ -1028,6 +1028,11 @@ impl Player {
                 }
                 self.world().await.respawn_player(self, false).await;
 
+                let screen_handler = self.current_screen_handler.lock().await;
+                let mut screen_handler = screen_handler.lock().await;
+                screen_handler.sync_state().await;
+                drop(screen_handler);
+
                 // Restore abilities based on gamemode after respawn
                 let mut abilities = self.abilities.lock().await;
                 abilities.set_for_gamemode(self.gamemode.load());
